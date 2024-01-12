83beddfdb9bd30b6bbbbcc2e18ec86e7e05dc1cc
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-01-12 12:38:25 +0300

    1

---
+++ seminar/1/script.js
@@ -0,0 +1,15 @@
+/* <div id="block">
+	<p>1</p>
+	<p>2</p>
+</div>
+Получите ссылку на первый абзац из дива с id, равным block, выведите его в консоль
+Получите ссылку на первый абзац с классом www. и вывести его в консоль
+<p class="www">text 1</p>
+<p class="www">text 2</p> */
+
+const blok = document.querySelector("#block");
+const firstChild = blok.children[0];
+console.log(firstChild);
+
+const tex = document.querySelector(".www");
+console.log(tex);
