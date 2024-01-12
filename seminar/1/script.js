f5c7d1630dc7ca63dd87dac70b421ad1b75abaa3
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-01-12 14:57:26 +0300

    3

--- seminar/1/script.js
+++ seminar/1/script.js
@@ -27,3 +27,15 @@ lin.href = "https://developer.mozilla.org/ru";
 const phot = document.querySelector(".photo");
 phot.src =
   "/seminar/1/img/1662431382_14-kartinkof-club-p-novie-i-krasivie-kartinki-puma-20.jpg";
+
+/*Дан тег <div class="content"></div> 
+Создайте новый элемент p
+Добавьте в него текст “Новый текстовый элемент”
+Добавьте созданный элемент внутри <div class="content"></div>
+Удалите добавленный узел */
+
+const conten = document.querySelector(".content");
+const paragraphElement = document.createElement("p");
+paragraphElement.textContent = "Новый текстовый элемент";
+conten.appendChild(paragraphElement);
+paragraphElement.remove();
