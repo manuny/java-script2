95295128ded33f165ba0220e79de3f753216a6a4
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-01-13 12:54:05 +0300

    №3

--- homework/1/script.js
+++ homework/1/script.js
@@ -2,9 +2,13 @@
 super_linkElement = document.getElementById("super_link");
 console.log(super_linkElement);
 //2. Внутри всех элементов на странице, которые имеют класс "card-link", поменяйте текст внутри элемента на "ссылка".
-
+cart_linkElement = document.querySelectorAll(".card-link");
+cart_linkElement.forEach((element) => {
+  element.textContent = "ссылка";
+});
 //3. Найти все элементы на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывести полученную коллекцию в консоль.
-
+const elems = document.querySelectorAll(".card-body .card-link");
+console.log(elems);
 //4. Найти первый попавшийся элемент на странице у которого есть атрибут data-number со значением 50 и вывести его в консоль.
 
 //5. Выведите содержимое тега title в консоль.
