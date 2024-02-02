d455bc65243579f98bf7218d036bc70fc3014571
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-02-03 01:27:49 +0300

    Задание 4

--- seminar/3/4/script.js
+++ seminar/3/4/script.js
@@ -4,10 +4,21 @@
 Создать кнопку, при клике на которую ей добавляется класс “click” */
 
 const menuELement = document.querySelector(".menu");
-const divButton = document.querySelector(".button");
+const divButton1 = document.querySelector(".button1");
+const divButton2 = document.querySelector(".button2");
+const divButton3 = document.querySelector(".button3");
 
-divButton.addEventListener("click", function (e) {
+divButton1.addEventListener("click", function (e) {
   const menuEl = document.createElement("li");
   menuEl.textContent = "новый элемент списка";
   menuELement.appendChild(menuEl);
+  menuEl.classList.add("item");
+});
+
+divButton2.addEventListener("click", function (e) {
+  const itemEl = document.querySelector(".item");
+  itemEl.remove();
+});
+divButton3.addEventListener("click", function (e) {
+  divButton3.classList.add("click");
 });
