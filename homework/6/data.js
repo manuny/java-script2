32045b1096db95771fb5d62c4d764ce6f57fcfef
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-02-25 01:54:58 +0300

    шаблон

--- homework/6/data.js
+++ homework/6/data.js
@@ -1,53 +1,51 @@
-const dataProducts = ` 
-[
+const products = `
+ [
   {
-   "id": 1,
-   "name": "ELLERY X M'O CAPSULE",
-   "titel": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-   "currency: "USD",
-   "price": 52,
-   "imge": "./img/product1.svg"
+    "id": 1,
+    "name": "ELLERY X M'O CAPSULE",
+    "titel":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52",
+	"imgProduct": "imge/1.jpg"
   },
   {
     "id": 2,
     "name": "ELLERY X M'O CAPSULE",
-    "titel": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "currency: "USD",
-    "price": 32,
-    "imge": "./img/product2.svg"
-   },
-   {
+    "titel":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$32",
+    "imgProduct": "imge/2.jpg"
+  },
+  {
     "id": 3,
     "name": "ELLERY X M'O CAPSULE",
-    "titel": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "currency: "USD",
-    "price": 50,
-    "imge": "./img/product3.svg"
-   },
-   {
+    "titel":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$50",
+    "imgProduct": "imge/3.jpg"
+  },
+  {
     "id": 4,
     "name": "ELLERY X M'O CAPSULE",
-    "titel": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "currency: "USD",
-    "price": 52,
-    "imge": "./img/product4.svg"
-   },
-   {
+    "titel":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52",
+    "imgProduct": "imge/4.jpg"
+  },
+  {
     "id": 5,
     "name": "ELLERY X M'O CAPSULE",
-    "titel": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "currency: "USD",
-    "price": 55,
-    "imge": "./img/product5.svg"
-   },
-   {
+    "titel":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$55",
+    "imgProduct": "imge/5.jpg"
+  },
+  {
     "id": 6,
     "name": "ELLERY X M'O CAPSULE",
-    "titel": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-    "currency: "USD",
-    "price": 60,
-    "imge": "./img/product6.svg"
-   }
-  
-]
-`;
+    "titel":
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$60",
+    "imgProduct": "imge/6.jpg"
+  }
+]`;
--- homework/6/script.js
+++ homework/6/script.js
@@ -1,5 +1,35 @@
-const data = JSON.parse(dataInfo);
+const data = JSON.parse(products);
 
-const contentBoxEl = document.querySelector(".content");
+const productBoxEl = document.querySelector(".product-box__content");
 
-data.forEach(({}) => {});
+data.forEach(({ imgProduct, name, titel, price }) => {
+  const productEl = document.createElement("div");
+  productEl.className = "product";
+  productBoxEl.append(productEl);
+
+  const productImageEl = document.createElement("img");
+  productImageEl.classList.add = "product__img";
+  productImageEl.setAttribute("src", imgProduct);
+  productImageEl.setAttribute("alt", "foto");
+  productBoxEl.append(productImageEl);
+
+  const productContEl = document.createElement("div");
+  productEl.className = "product__content";
+  productBoxEl.append(productContEl);
+
+  const linkEl = document.createElement("a");
+  linkEl.classList.add = "product__heading";
+  linkEl.href = "#";
+  linkEl.textContent = name;
+  productContEl.append(linkEl);
+
+  const tittelEl = document.createElement("p");
+  tittelEl.classList.add = "product__text";
+  tittelEl.textContent = titel;
+  productContEl.append(tittelEl);
+
+  const praceEl = document.createElement("p");
+  praceEl.classList.add = "product__text";
+  praceEl.textContent = price;
+  productContEl.append(praceEl);
+});
